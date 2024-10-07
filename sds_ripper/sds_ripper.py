acb6a758c73645d4023e23dc2f5130816e7bc29d
Mohammed Hussain <mah5@ualberta.ca>
2024-10-07 11:06:25 -0700

    init setup

---
+++ sds_ripper/sds_ripper.py
@@ -0,0 +1,12 @@
+import os
+import pymupdf
+print(pymupdf.__doc__)
+# general idea: if match in common field title: check line and line under, maybe regex between field title x and next field title y? 
+
+def main():
+    """iterate over files in sds folder, parse and generate a csv file where each row is a sds file data"""
+    # for sds in 
+
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
+    main()
