f612d50c2e83ebd7e767299a2acd909031293d45
Mohammed Hussain <mah5@ualberta.ca>
2024-10-07 13:40:14 -0700

    using commit script to working branch

---
+++ activate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# requirements:
+
+# python -m venv pymupdf-venv
+# . pymupdf-venv/bin/activate
+# python -m pip install --upgrade pip
+
+# installation:
+# pip install --upgrade pymupdf
+
+# cmd:
+. sds_ripper/pymupdf-venv/bin/activate
