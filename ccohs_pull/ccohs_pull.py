f8817a5022dd910f5bba4ab4b0706031a3946e87
Mohammed Hussain <mah5@ualberta.ca>
2024-10-07 14:44:38 -0700

    using commit script to working branch

--- ccohs_pull/ccohs_pull.py
+++ ccohs_pull/ccohs_pull.py
@@ -1,4 +1,2 @@
 # This will pull all english pdfs from the website
 # selenium headless browser requied, approximately 62k pdfs required, need small sample size to test pymupdf script, fisher, sigma, bioshop, bd, thermo
-# nrm and tdg
-# tdg/icac/dot
