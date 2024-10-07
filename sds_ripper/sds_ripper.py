bb451853d28560ad201385098c14f49366d4a132
Mohammed Hussain <mah5@ualberta.ca>
2024-10-07 12:53:42 -0700

    using commit script to working branch

--- sds_ripper/sds_ripper.py
+++ sds_ripper/sds_ripper.py
@@ -3,10 +3,15 @@ import pymupdf
 print(pymupdf.__doc__)
 # general idea: if match in common field title: check line and line under, maybe regex between field title x and next field title y? 
 
+'''
+TO PULL:
+chemical name, synonyms, chemical forumula, flash point, density weight, melting point, boiling point, molecular weight, pH value, unit, physical state, compatability category, special hazard, UN number, UN pack group, TDG primary, TDG secondary, TDG teritary, storage requirements, GHS classification, GHS hazard statements, GHS signal word, GHS precautionary statement codes, GHS pictogram(s), NFPA Diamond, DOT Guide
+'''
+
 def main():
-    """iterate over files in sds folder, parse and generate a csv file where each row is a sds file data"""
+    '''iterate over files in sds folder, parse and generate a csv file where each row is a sds file data'''
     # for sds in 
 
 if __name__ == "__main__":
-    """This is executed when run from the command line"""
+    '''This is executed when run from the command line'''
     main()
