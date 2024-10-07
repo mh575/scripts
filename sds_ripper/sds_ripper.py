c4ceb4d00d6ccb43fe8d914941e18f97156e7043
Mohammed Hussain <mah5@ualberta.ca>
2024-10-07 14:42:57 -0700

    using commit script to working branch

--- sds_ripper/sds_ripper.py
+++ sds_ripper/sds_ripper.py
@@ -5,12 +5,25 @@ print(pymupdf.__doc__)
 
 '''
 TO PULL:
-chemical name, synonyms, chemical forumula, flash point, density weight, melting point, boiling point, molecular weight, pH value, unit, physical state, compatability category, special hazard, UN number, UN pack group, TDG primary, TDG secondary, TDG teritary, storage requirements, GHS classification, GHS hazard statements, GHS signal word, GHS precautionary statement codes, GHS pictogram(s), NFPA Diamond, DOT Guide
+chemical name, synonyms, forumula, flash point, density, melting point, boiling point, molecular weight, pH value, physical state, compatability category(?), UN number, UN pack group, TDG primary, TDG secondary, TDG teritary, GHS classification, GHS hazard statements (pictogram(s) derived from h-codes), GHS signal word, GHS precautionary statement codes, NFPA Diamond, DOT Guide(?)
 '''
 
 def main():
     '''iterate over files in sds folder, parse and generate a csv file where each row is a sds file data'''
-    # for sds in 
+    assert os.path.exists(os.getcwd()+'/sds_files')
+    
+    dir = os.fsencode(os.getcwd()+'/sds_files')
+    
+    for file in os.listdir(dir):
+         filename = os.fsdecode(file)
+         if filename.endswith('.pdf'):
+             pass
+
+         else: # file is not a pdf
+            if not (filename == '.gitignore'):
+                raise ValueError('file extension is not pdf: '+filename)
+            else:
+                continue
 
 if __name__ == "__main__":
     '''This is executed when run from the command line'''
